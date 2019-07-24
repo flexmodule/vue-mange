ec0e0ec5a5651fc2230e35561a2b8f4da4be9f3d
zhuohai <1181316781@qq.com>
2019-07-24 23:17:53 +0800

    修改为css

--- src/views/booksubmit.vue
+++ src/views/booksubmit.vue
@@ -1,141 +1,146 @@
-<style lang="scss" scope>
-.booksubmit {
-	.header {
-		position: relative;
-		background: #d74d33;
-		text-align: center;
-		padding: .2rem 0;
-		.header-l {
-			position: absolute;
-			left: .2rem;
-			top: .3rem;
-			font-size: 0;
-        width: 0.3rem;
-				height: 0.3rem;
-				transform: rotate(180deg);
-        img {
-          width: 100%;
-          height: 100%;
-        }
-		}
-		.header-r {
-			font: .3rem/.5rem "";
-			color: #fff;
-		}
-	}
-	.body {
-		padding: .3rem .6rem;
-		.data {
-			.data-t {
-				font: .3rem/.7rem "";
-				color: #999;
-			}
-			.data-b {
-				padding-left: 2rem;
-				display: flex;
-				justify-content: space-between;
-				.data-bl {
-					font: .3rem/.4rem "";
-					color: #000;
-					margin-right: .8rem;
-				}
-				.data-br {
-					width: .4rem;
-					height: .4rem;
-					font-size: 0;
-					img {
-						width: 100%;
-						height: 1005;
-					}
-				}
-			}
-			.te {
-				padding: 0;
-				textarea {
-					width: 100%;
-					border: none;
-					height: 2rem;
-					padding-left: .2rem;
-					&:focus {
-						border: none;
-						outline: none;
-					}
-				}
-			}
-		}
-	}
-	.submit {
-		width: 3.4rem;
-		text-align: center;
-		color: #fff;
-		font: .3rem/.6rem "";
-		margin: .2rem auto;
-		background: #d74d33;
-		border-radius: .3rem;
-	}
+<style scope>
+.booksubmit .header {
+  position: relative;
+  background: #d74d33;
+  text-align: center;
+  padding: 0.2rem 0;
+}
+.booksubmit .header .header-l {
+  position: absolute;
+  left: 0.2rem;
+  top: 0.3rem;
+  font-size: 0;
+  width: 0.3rem;
+  height: 0.3rem;
+  transform: rotate(180deg);
+}
+.booksubmit .header .header-l img {
+  width: 100%;
+  height: 100%;
+}
+.booksubmit .header .header-r {
+  font: 0.3rem/.5rem "";
+  color: #fff;
+}
+.booksubmit .body {
+  padding: 0.3rem 0.6rem;
+}
+.booksubmit .body .data .data-t {
+  font: 0.3rem/.7rem "";
+  color: #999;
+}
+.booksubmit .body .data .data-b {
+  padding-left: 2rem;
+  display: flex;
+  justify-content: space-between;
+}
+.booksubmit .body .data .data-b .data-bl {
+  font: 0.3rem/.4rem "";
+  color: #000;
+  margin-right: 0.8rem;
+}
+.booksubmit .body .data .data-b .data-br {
+  width: 0.4rem;
+  height: 0.4rem;
+  font-size: 0;
+}
+.booksubmit .body .data .data-b .data-br img {
+  width: 100%;
+  height: 100%;
+}
+.booksubmit .body .data .te {
+  padding: 0;
+}
+.booksubmit .body .data .te textarea {
+  width: 100%;
+  border: none;
+  height: 2rem;
+  padding-left: 0.2rem;
+}
+.booksubmit .body .data .te textarea:focus {
+  border: none;
+  outline: none;
+}
+.booksubmit .submit {
+  width: 3.4rem;
+  text-align: center;
+  color: #fff;
+  font: 0.3rem/.6rem "";
+  margin: 0.2rem auto;
+  background: #d74d33;
+  border-radius: 0.3rem;
 }
 </style>
 <template>
   <div class="booksubmit">
     <div class="header">
-			<div class="header-l" @click="returnmeeting"><img src="@/assets/img/arrow.png" alt=""></div>
-			<div class="header-r">预定提交</div>
-		</div>
-		<div class="body">
-			<div class="data">
-				<div class="data-t">预定会议室</div>
-				<div class="data-b">
-					<div class="data-bl">9s-908</div>
-					<div class="data-br"><img src="@/assets/img/selectarrow.png" alt=""></div>
-				</div>
-			</div>
-			<div class="data">
-				<div class="data-t">预定日期</div>
-				<div class="data-b">
-					<div class="data-bl">2017-03</div>
-					<div class="data-br"><img src="@/assets/img/selectarrow.png" alt=""></div>
-				</div>
-			</div>
-			<div class="data">
-				<div class="data-t">开始时间</div>
-				<div class="data-b">
-					<div class="data-bl">08:30</div>
-					<div class="data-br"><img src="@/assets/img/selectarrow.png" alt=""></div>
-				</div>
-			</div>
-			<div class="data">
-				<div class="data-t">结束时间</div>
-				<div class="data-b">
-					<div class="data-bl">12:00</div>
-					<div class="data-br"><img src="@/assets/img/selectarrow.png" alt=""></div>
-				</div>
-			</div>
-			<div class="data">
-				<div class="data-t">预定会议室</div>
-				<div class="data-b te"><textarea name="" id="" cols="30" rows="10">会议用途在此输入</textarea>	</div>
-			</div>
-		</div>
-		<div class="submit">提交</div>
+      <div class="header-l" @click="returnmeeting">
+        <img src="@/assets/img/arrow.png" alt>
+      </div>
+      <div class="header-r">预定提交</div>
+    </div>
+    <div class="body">
+      <div class="data">
+        <div class="data-t">预定会议室</div>
+        <div class="data-b">
+          <div class="data-bl">9s-908</div>
+          <div class="data-br">
+            <img src="@/assets/img/selectarrow.png" alt>
+          </div>
+        </div>
+      </div>
+      <div class="data">
+        <div class="data-t">预定日期</div>
+        <div class="data-b">
+          <div class="data-bl">2017-03</div>
+          <div class="data-br">
+            <img src="@/assets/img/selectarrow.png" alt>
+          </div>
+        </div>
+      </div>
+      <div class="data">
+        <div class="data-t">开始时间</div>
+        <div class="data-b">
+          <div class="data-bl">08:30</div>
+          <div class="data-br">
+            <img src="@/assets/img/selectarrow.png" alt>
+          </div>
+        </div>
+      </div>
+      <div class="data">
+        <div class="data-t">结束时间</div>
+        <div class="data-b">
+          <div class="data-bl">12:00</div>
+          <div class="data-br">
+            <img src="@/assets/img/selectarrow.png" alt>
+          </div>
+        </div>
+      </div>
+      <div class="data">
+        <div class="data-t">预定会议室</div>
+        <div class="data-b te">
+          <textarea name id cols="30" rows="10">会议用途在此输入</textarea>
+        </div>
+      </div>
+    </div>
+    <div class="submit">提交</div>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'booksubmit',
+  name: "booksubmit",
   components: {},
   data() {
     return {
-			roomnum: "9S-908",
-		
+      roomnum: "9S-908"
     };
   },
   methods: {
-		returnmeeting() {
-			this.$router.push("/");
-		}
+    returnmeeting() {
+      this.$router.push("/");
+    }
   },
   mounted() {}
-}
+};
 </script>
--- src/views/mybooking.vue
+++ src/views/mybooking.vue
@@ -1,136 +1,134 @@
-<style lang="scss" scope>
-.mybooking {
-  .header {
-    position: relative;
-    background: #d74d33;
-    padding: 0.1rem 0 0.1rem 0;
-    color: #fff;
-    .header-l {
-      position: absolute;
-      left: 0.2rem;
-      top: 0.35rem;
-      font-size: 0;
-      width: 0.3rem;
-      height: 0.3rem;
-      transform: rotate(180deg);
-      img {
-        width: 100%;
-        height: 100%;
-      }
-    }
-    .header-c {
-      text-align: center;
-      font: 0.3rem/0.8rem "";
-    }
-    .header-r {
-      position: absolute;
-      top: 0.3rem;
-      right: 0.2rem;
-      display: flex;
-      align-items: center;
-      .header-rl {
-        font: 0.2rem/0.4rem "";
-        margin-right: 0.1rem;
-      }
-      .header-rr {
-        font-size: 0;
-        width: 0.3rem;
-        height: 0.3rem;
-        img {
-          width: 100%;
-          height: 100%;
-        }
-      }
-    }
-	}
-	.info {
-		display: flex;
-		justify-content: center;
-		font: .32rem/.7rem "";
-		.info-l {
-			color: #999;
-			margin-right: .2rem;
-		}
-		.info-r {
-			color: #000;
-		}
-	}
-	.body {
-		padding: 0.4rem .8rem 0;
-		.data {
-			margin-bottom: .3rem;
-			position: relative;
-			.del {
-				position: absolute;
-				right: .3rem;
-				top: 0;
-				font: .24rem/.24rem "";
-				color: #4fbbf0;
-			}
-			.data-t {
-				font: .3rem/.5rem "";
-			}
-			.data-c {
-				color: #999;
-				font: .24rem/.5rem "";
-			}
-			.data-b {
-				display: flex;
-				justify-content: space-between;
-				font: .22rem/.5rem "";
-				color: #999;
-			}
-			&:before {
-				position: absolute;
-				width: .3rem;
-				height: .3rem;
-				content: "";
-				background: #d74d33;
-				left: -.5rem;
-				border-radius: 50%;
-				top: .1rem;
-			}
-		}
-		.other {
-			text-align: center;
-			font: .3rem/.4rem "";
-			color: #333;
-		}
-	}
+<style scope>
+.mybooking .header {
+  position: relative;
+  background: #d74d33;
+  padding: 0.1rem 0 0.1rem 0;
+  color: #fff;
+}
+.mybooking .header .header-l {
+  position: absolute;
+  left: 0.2rem;
+  top: 0.35rem;
+  font-size: 0;
+  width: 0.3rem;
+  height: 0.3rem;
+  transform: rotate(180deg);
+}
+.mybooking .header .header-l img {
+  width: 100%;
+  height: 100%;
+}
+.mybooking .header .header-c {
+  text-align: center;
+  font: 0.3rem/0.8rem "";
+}
+.mybooking .header .header-r {
+  position: absolute;
+  top: 0.3rem;
+  right: 0.2rem;
+  display: flex;
+  align-items: center;
+}
+.mybooking .header .header-rl {
+  font: 0.2rem/0.4rem "";
+  margin-right: 0.1rem;
+}
+.mybooking .header .header-rr {
+  font-size: 0;
+  width: 0.3rem;
+  height: 0.3rem;
+}
+.mybooking .header .header-rr img {
+  width: 100%;
+  height: 100%;
+}
+.mybooking .info {
+  display: flex;
+  justify-content: center;
+  font: 0.32rem/.7rem "";
+}
+.mybooking .info .info-l {
+  color: #999;
+  margin-right: 0.2rem;
+}
+.mybooking .info .info-r {
+  color: #000;
+}
+.mybooking .body {
+  padding: 0.4rem 0.8rem 0;
+}
+.mybooking .body .data {
+  margin-bottom: 0.3rem;
+  position: relative;
+}
+.mybooking .body .data .del {
+  position: absolute;
+  right: 0.3rem;
+  top: 0;
+  font: 0.24rem/.24rem "";
+  color: #4fbbf0;
+}
+.mybooking .body .data .data-t {
+  font: 0.3rem/.5rem "";
+}
+.mybooking .body .data .data-c {
+  color: #999;
+  font: 0.24rem/.5rem "";
+}
+.mybooking .body .data .data-b {
+  display: flex;
+  justify-content: space-between;
+  font: 0.22rem/.5rem "";
+  color: #999;
+}
+.mybooking .body .data:before {
+  position: absolute;
+  width: 0.3rem;
+  height: 0.3rem;
+  content: "";
+  background: #d74d33;
+  left: -0.5rem;
+  border-radius: 50%;
+  top: 0.1rem;
+}
+.mybooking .body .other {
+  text-align: center;
+  font: 0.3rem/.4rem "";
+  color: #333;
 }
 </style>
 <template>
   <div class="mybooking">
     <div class="header">
       <div class="header-l" @click="returnmeeting">
-        <img src="@/assets/img/arrow.png" alt />
+        <img src="@/assets/img/arrow.png" alt>
       </div>
       <div class="header-c">我的预订</div>
       <div class="header-r" @click="returnmeeting">
         <div class="header-rl">会议室预定</div>
         <div class="header-rr">
-          <img src="@/assets/img/arrow.png" alt />
+          <img src="@/assets/img/arrow.png" alt>
         </div>
       </div>
     </div>
-		<div class="info">
-			<div class="info-l">温馨提示</div>
-			<div class="info-r">您最近提交了{{notes.length}}条预定记录</div>
-		</div>
-		<div class="body">
-			<div v-if="notes.length!=0">
-				<div class="data" v-for="item,index in notes">
-				<div class="data-t">{{item.room}}</div>
-				<div class="data-c">{{item.reasons}}</div>
-				<div class="data-b">
-					<div class="data-bl">{{item.time}}</div>
-					<div class="data-br">{{item.date}}</div>
-				</div>
-				<div class="del" @click="del(index)">删除</div>
-			</div>
-			</div>
-			<div class="other" v-else>对不起，目前没有任何记录</div>
-		</div>
+    <div class="info">
+      <div class="info-l">温馨提示</div>
+      <div class="info-r">您最近提交了{{notes.length}}条预定记录</div>
+    </div>
+    <div class="body">
+      <div v-if="notes.length!=0">
+        <div class="data" v-for="item,index in notes">
+          <div class="data-t">{{item.room}}</div>
+          <div class="data-c">{{item.reasons}}</div>
+          <div class="data-b">
+            <div class="data-bl">{{item.time}}</div>
+            <div class="data-br">{{item.date}}</div>
+          </div>
+          <div class="del" @click="del(index)">删除</div>
+        </div>
+      </div>
+      <div class="other" v-else>对不起，目前没有任何记录</div>
+    </div>
   </div>
 </template>
 
@@ -140,15 +138,27 @@ export default {
   components: {},
   data() {
     return {
-			notes:[{room:"9S-908",reasons:'项目组新方案设计推广介绍',time:'09:30-10:00',date:'2017-03-08'},
-			{room:"9S-9087",reasons:'项目组新方案设计推广介绍',time:'09:30-10:00',date:'2017-03-08'},]
+      notes: [
+        {
+          room: "9S-908",
+          reasons: "项目组新方案设计推广介绍",
+          time: "09:30-10:00",
+          date: "2017-03-08"
+        },
+        {
+          room: "9S-9087",
+          reasons: "项目组新方案设计推广介绍",
+          time: "09:30-10:00",
+          date: "2017-03-08"
+        }
+      ]
     };
   },
   methods: {
-		del(index) {
-			this.notes.splice(index,1);
-			this.$forceUpdate()
-		},
+    del(index) {
+      this.notes.splice(index, 1);
+      this.$forceUpdate();
+    },
     returnmeeting() {
       this.$router.push("/");
     }
